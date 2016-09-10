b02d5a8654f4971de6f96bc65355de6b3c355899
aafaque33 <aafaqueabdullah@yahoo.com>
2016-09-10 14:18:53 -0500

    Changes in Python script file for finding Nth node value to the last node

--- NthtoLastNode/NthtoLastNode.py
+++ NthtoLastNode/NthtoLastNode.py
@@ -1,24 +1,10 @@
 
-# coding: utf-8
-
-# # Nth to the Last Node
-# 
-# ## Problem Statement
-# 
-# Write a function that takes a head node and an integer value n and then returns the nth to last node in the linked list. For example, given:
-# 
-# ## Solution
-
-# In[2]:
-
 class Node:
 
     def __init__(self, value):
         self.value = value
         self.nextnode  = None
 
-
-# In[3]:
 
 def nth_to_last_node(n, head):
     firstnode = head
@@ -32,46 +18,3 @@ def nth_to_last_node(n, head):
         secondnode = secondnode.nextnode
         
     return(secondnode)
-
-
-# # Test Cases using Nose tools
-
-# In[5]:
-
-"""
-RUN THIS CELL TO TEST YOUR SOLUTION AGAINST A TEST CASE 
-
-PLEASE NOTE THIS IS JUST ONE CASE
-"""
-
-from nose.tools import assert_equal
-
-a = Node(1)
-b = Node(2)
-c = Node(3)
-d = Node(4)
-e = Node(5)
-
-a.nextnode = b
-b.nextnode = c
-c.nextnode = d
-d.nextnode = e
-
-####
-
-class TestNLast(object):
-    
-    def test(self,sol):
-        
-        assert_equal(sol(2,a),d)
-        print('ALL TEST CASES PASSED')
-        
-# Run tests
-t = TestNLast()
-t.test(nth_to_last_node)
-
-
-# In[ ]:
-
-
-
